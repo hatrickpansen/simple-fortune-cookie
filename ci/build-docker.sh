44ac48aa789cc1bfcde6f9bbea2a6f7499513c90
Patrick Hansen <patrickhansen41@gmail.com>
2021-08-19 11:26:58 +0000

    modiefied build-docker.sh and push-docker.sh to support simple-fortune-cookie

--- ci/build-docker.sh
+++ ci/build-docker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 [[ -z "${GIT_COMMIT}" ]] && Tag='local' || Tag="${GIT_COMMIT::4}"
 [[ -z "${docker_username}" ]] && DockerRepo='' || DockerRepo="${docker_username}/"
-docker build -t "${DockerRepo}simple-fortune-cookie:latest" -t "${DockerRepo}simple-fortune-cookie:1.0-$Tag" app/
+docker build -t "${DockerRepo}simple-fortune-cookie_frontend:latest" -t "${DockerRepo}simple-fortune-cookie_frontend:1.0-$Tag" backend/
+docker build -t "${DockerRepo}simple-fortune-cookie_backend:latest" -t "${DockerRepo}simple-fortune-cookie-backend:1.0-$Tag" frontend/
--- ci/push-docker.sh
+++ ci/push-docker.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 echo "$docker_password" | docker login --username "$docker_username" --password-stdin
-docker push "$docker_username/simple-fortune-cookie1.0-${GIT_COMMIT::4}" 
-docker push "$docker_username/simple-fortune-cookie:latest" &
+docker push "$docker_username/simple-fortune-cookie_frontend:1.0-${GIT_COMMIT::4}" 
+docker push "$docker_username/simple-fortune-cookie_frontend:latest" &
+wait
+
+echo "$docker_password" | docker login --username "$docker_username" --password-stdin
+docker push "$docker_username/simple-fortune-cookie_backend:1.0-${GIT_COMMIT::4}" 
+docker push "$docker_username/simple-fortune-cookie_backend:latest" &
 wait
